ca2f3d1e27fb7b2bad7778c284b238f1a79cc44a
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-01-09 20:27:24 +0100

    setup.py

--- src/tui/setup.py
+++ src/tui/setup.py
@@ -12,7 +12,7 @@ setup(
     long_description_content_type='text/markdown',
     url="https://github.com/denismaggior8/enigma-tui",
     name="enigmatui",
-    version="1.0.0",
+    version="1.1.0",
     include_package_data=True,
     packages=find_packages(
         # All keyword arguments below are optional:
@@ -24,7 +24,7 @@ setup(
         ],
     },
     install_requires=[
-        "textual",
+        "textual==1.0.0",
         "enigmapython==1.2.0"
     ],
     description="Enigma TUI is a Terminal User Interface for Enigma machines, allowing you to simulate different Enigma machine models from the terminal"
