b18f2ffa391e025a4b3dec149f5cb509698c5d21
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-01-09 11:09:12 +0100

    debug config

--- src/tui/enigmatui/data/enigma_config.py
+++ src/tui/enigmatui/data/enigma_config.py
@@ -6,14 +6,15 @@ class EnigmaConfig(Observable):
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(EnigmaConfig, cls).__new__(cls)
-            cls._instance.configured_enigma = None  # valore predefinito
-            cls._instance.enigma = None  # valore predefinito
             cls._instance._initialized = False
         return cls._instance
 
     def __init__(self):
         if not self._initialized:
             super().__init__()  # Initialize the observer
+            self.configured_enigma = None  # 
+            self.enigma = None 
+            self._initialized = True
     
     def set_configured_enigma(self, enigma):
         self.configured_enigma = enigma
@@ -27,6 +28,7 @@ class EnigmaConfig(Observable):
     def reset_enigma(self):
         self.enigma = self.configured_enigma.clone()
         self.notify_observers(self,None,None)
+    
     
    
    
