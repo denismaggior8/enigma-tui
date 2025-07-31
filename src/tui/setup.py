0faf03e6c89a8ad2f4fc2292e95e85057e694ceb
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-07-31 18:19:45 +0200

    fix install_requires enigmapython==1.3.0

--- src/tui/setup.py
+++ src/tui/setup.py
@@ -36,7 +36,7 @@ setup(
     },
     install_requires=[
         "textual==1.0.0",
-        "enigmapython==1.2.3"
+        "enigmapython==1.3.0"
     ],
     description="Enigma TUI is a Terminal User Interface for Enigma machines, allowing you to simulate different Enigma machine models from the terminal"
 )
